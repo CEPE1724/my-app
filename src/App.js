d6c67a2ed582808a84fa9c749114beb5e5fedebe
CEPE1724 <edisonnacional1@gmail.com>
2024-06-03 10:40:22 -0500

    creacion de rutas

--- src/App.js
+++ src/App.js
@@ -3,23 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1 class="text-2xl bg-red-800 text-white ww-2/3" >hola mundo </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="about" element={<About />} />
+    <Route path="dashboard" element={<Dashboard />} />
+   </Routes>
   );
 }
 
